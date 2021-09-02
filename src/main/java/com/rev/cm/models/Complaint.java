b6b51b81357791eee43aca012f06fb3cc6a27fab
FBThornton <97fthornton@gmail.com>
2021-09-02 15:57:29 -0400

    Added auto generated users and complaint into database

--- src/main/java/com/rev/cm/models/Complaint.java
+++ src/main/java/com/rev/cm/models/Complaint.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Setter
 public class Complaint {
 
+	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
--- src/main/java/com/rev/cm/models/User.java
+++ src/main/java/com/rev/cm/models/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @Setter
 public class User {
 
+	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
