0e20a0eb7bce7fdd5a8738c6e70d98636199c511
FBThornton <97fthornton@gmail.com>
2021-09-02 15:58:05 -0400

    Added import script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into users values('1', 'true', 'emp', 'emp');
+insert into users values('2', 'false', 'user', 'user');
+
+insert into complaints values('1','Complaining','Just dont','1','My complaint','2');
